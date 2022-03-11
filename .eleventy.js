aafb2ff9025a24e28199259645a1608ea058d760
drjw <danielrjaywagner@gmail.com>
2022-03-10 19:26:06 -0500

    4

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,18 @@
+const { DateTime } = require('luxon');
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/styles/');
   eleventyConfig.addWatchTarget('./src/styles/');
+
   eleventyConfig.addPassthroughCopy('./src/assets/');
   eleventyConfig.addWatchTarget('./src/assets/');
+
+  eleventyConfig.addShortcode('year', () => `${new Date().getFullYear()}`);
+
+  eleventyConfig.addFilter('postDate', (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
+
   return {
     dir: {
       input: 'src',
